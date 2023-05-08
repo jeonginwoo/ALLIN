acad636ba01b62c8253e4189c769d493ec50b8bb
nuEnu <96004205+jeonginwoo@users.noreply.github.com>
2023-05-08 19:09:57 +0900

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,29 @@
 <template>
-  <v-container>
-    <h1>조성현</h1>
+  <v-container class="bg-surface-variant">
+    <v-row no-gutters>
+      <v-col class="ma-2" cols="4">
+        <v-card>
+          <v-sheet class="pa-2 ma-2">
+            건수
+          </v-sheet>
+        </v-card>
+      </v-col>
+      <v-col class="ma-2">
+        <v-card>
+          <v-sheet class="pa-2 ma-2">
+            차트
+          </v-sheet>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col class="ma-2">
+        <v-card>
+          <v-sheet class="pa-2 ma-2">
+            프로젝트 리스트
+          </v-sheet>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
