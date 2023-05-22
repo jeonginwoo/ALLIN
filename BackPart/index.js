f47e320435e7e16d6f2e6ba8051c0b392232fccd
NDK <80230626+mark3473@users.noreply.github.com>
2023-05-22 21:42:10 +0900

    my project query문 수정

--- BackPart/index.js
+++ BackPart/index.js
@@ -81,7 +81,7 @@ app.post("/api/project_delete", (req, res) => {
 // 이용자의 참여 프로젝트 리스트 반환 ++ 현재 로그인중인 user_no를 front에서 보내주는것 구현 필요!!!!
 app.post("/api/my_project", (req, res) => {
   const data = req.body
-  database.query("SELECT Pno, Pname, case when(deadline-date(now()) < 0) then '지연' when (progress = 0) then '미착수' when (progress between 1 and 6) then '진행중' when (progress = 7) then '완료' end as state, progress, DATE_FORMAT(start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(end_date,'%Y.%m.%d')as end_date FROM works_on, project LEFT JOIN user ON project.mgr = user.user_no where works_on.user_no = ?", [data.user_no], (err, projects) => {
+  database.query("SELECT distinct project.Pno, Pname, case when(deadline-date(now()) < 0) then '지연' when (progress = 0) then '미착수' when (progress between 1 and 6) then '진행중' when (progress = 7) then '완료' end as state, progress, DATE_FORMAT(start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(end_date,'%Y.%m.%d')as end_date FROM works_on, project LEFT JOIN user ON project.mgr = user.user_no where works_on.user_no = ?", [data.user_no], (err, projects) => {
     if (err)
       console.log(err);
     else {
