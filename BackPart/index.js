a3cc4fca428bb3e2eed576da06d9b668ae0293e4
NDK <80230626+mark3473@users.noreply.github.com>
2023-05-23 21:00:28 +0900

    log 기능 위한 변경.
    back의 api 별로 주석에 달린 정보를 보내주세요!!!!!!!!!

--- BackPart/index.js
+++ BackPart/index.js
@@ -17,7 +17,7 @@ app.get("/api/user", (req, res) => {
 
 // 전체 프로젝트 리스트 반환. stae :: 진행단계가 0단계이면 미착수, 1~6단계이면 진행중, 7단계이면 완료, 현재 날짜가 deadline을 넘겼으면 지연.
 app.get("/api/project", (req, res) => {
-  database.query("SELECT Pno, Pname, user_name, case when(deadline-date(now()) < 0) then '지연' when (progress = 0) then '미착수' when (progress between 1 and 6) then '진행중' when (progress = 7) then '완료' end as state, progress, DATE_FORMAT(start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(end_date,'%Y.%m.%d')as end_date FROM project LEFT JOIN user ON project.mgr = user.user_no", (err, projects) => {
+  database.query("SELECT Pno, Pname, user_name, case when (progress = 7) then '완료' when(deadline-date(now()) < 0) then '지연' when (progress = 0) then '미착수' when (progress between 1 and 6) then '진행중' end as state, progress, DATE_FORMAT(start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(end_date,'%Y.%m.%d')as end_date FROM project LEFT JOIN user ON project.mgr = user.user_no", (err, projects) => {
     if (err)
       console.log(err);
     else {
@@ -26,10 +26,10 @@ app.get("/api/project", (req, res) => {
   })
 });
 
-// 신규 프로젝트 생성
+// 신규 프로젝트 생성 // *** 이용자의 user_no 보내줘야함 ***
 app.post("/api/project_create", (req, res) => {
   const data = req.body
-  database.query(`INSERT INTO project (Pname, mgr, progress, start_date, deadline, end_date) VALUES(?, ?, ?, ?, ?, null);`, [data.Pname, data.mgr, data.progress, data.start_date, data.deadline], (err, res) => {
+  database.query(`INSERT INTO project (Pname, mgr, progress, start_date, deadline, end_date) VALUES(?, ?, 1, date(now()), ?, null);`, [data.Pname, data.mgr, data.deadline], (err, res) => {
     if (err) {
       console.log(err)
     } else {
@@ -42,30 +42,36 @@ app.post("/api/project_create", (req, res) => {
               console.log(err)
             }
           })
+          database.query("INSERT INTO log (pnum, user_num, contents) VALUES(?, ?, '프로젝트 생성됌')", [res[0].Pno, data.user_no], (err, res) => {
+            if (err) console.log(err)
+          })
         }
       })
     }
   })
 });
 
-// 프로젝트 정보 갱신 + 진행 단계 갱신을 여기서 할것인가?
+// 프로젝트 정보 갱신 // *** 이용자의 user_no 보내줘야함 ***
 app.post("/api/project_update", (req, res) => {
   const data = req.body
-  database.query('SELECT mgr from project where Pno = ?', [data.Pno], (err, res) => {
+  database.query('SELECT mgr, progress from project where Pno = ?', [data.Pno], (err, res) => {
     if (!err) {
       database.query('UPDATE works_on SET user_no = ? where user_no = ? and pno = ?', [data.mgr, res[0].mgr, data.Pno], (err, res) => {
         if (err) console.log(err)
       })
+      database.query("INSERT INTO log (pnum, user_num, contents) VALUES(?, ?, '프로젝트 정보 갱신됌')", [data.Pno, data.user_no], (err, res) => {
+        if (err) console.log(err)
+      })
     } else console.log(err)
   })
-  database.query(`UPDATE project SET Pname=?, mgr=?, progress=?, start_date=?, deadline=? WHERE Pno=?`, [data.Pname, data.mgr, data.progress, data.start_date, data.deadline, data.Pno], (err, res) => {
+  database.query(`UPDATE project SET Pname=?, mgr=?, progress=?, deadline=? WHERE Pno=?`, [data.Pname, data.mgr, data.progress, data.deadline, data.Pno], (err, res) => {
     if (err) {
       console.log(err)
     }
   })
 });
 
-// 프로젝트 삭제
+// 프로젝트 삭제 // *** 이용자의 user_no 보내줘야함 ***
 app.post("/api/project_delete", (req, res) => {
   const data = req.body
   console.log(data)
@@ -73,15 +79,17 @@ app.post("/api/project_delete", (req, res) => {
     if (err) {
       console.log(err)
     } else {
-      console.log(res)
+      database.query("INSERT INTO log (pnum, user_num, contents) VALUES(?, ?, '프로젝트 삭제됌')", [data.Pno, data.user_no], (err, res) => {
+        if (err) console.log(err)
+      })
     }
   })
 });
 
-// 이용자의 참여 프로젝트 리스트 반환 ++ 현재 로그인중인 user_no를 front에서 보내주는것 구현 필요!!!!
+// 이용자의 참여 프로젝트 리스트 반환 // *** 이용자의 user_no 보내줘야함 ***
 app.post("/api/my_project", (req, res) => {
   const data = req.body
-  database.query("select p.Pno, p.Pname, u.user_name, p.deadline-date(now()) as d_day, case when(p.deadline-date(now()) < 0) then '지연' when (p.progress = 0) then '미착수' when (p.progress between 1 and 6) then '진행중' when (p.progress = 7) then '완료' end as state, p.progress, DATE_FORMAT(p.start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(p.deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(p.end_date,'%Y.%m.%d')as end_date from project p, user u, works_on w where w.pno = p.Pno and p.mgr = u.user_no and w.user_no = ?", [data.user_no], (err, projects) => {
+  database.query("select p.Pno, p.Pname, u.user_name, p.deadline-date(now()) as d_day, case when (p.progress = 7) then '완료' when(p.deadline-date(now()) < 0) then '지연' when (p.progress = 0) then '미착수' when (p.progress between 1 and 6) then '진행중' end as state, p.progress, DATE_FORMAT(p.start_date,'%Y.%m.%d')as start_date, DATE_FORMAT(p.deadline,'%Y.%m.%d')as deadline, DATE_FORMAT(p.end_date,'%Y.%m.%d')as end_date from project p, user u, works_on w where w.pno = p.Pno and p.mgr = u.user_no and w.user_no = ?", [data.user_no], (err, projects) => {
     if (err)
       console.log(err);
     else {
@@ -90,8 +98,33 @@ app.post("/api/my_project", (req, res) => {
   })
 });
 
-
-
+// // 대시보드 page 좌측 상단.
+// app.post("/api/project_of_year", (req, res) => {
+//   const data = req.body
+//   const result = {
+//     complete: 0,
+//     working: 0,
+//     delayed: 0
+//   }
+//   database.query("select count(progress) as complete from project where progress = 7 and year(deadline) = ?", [data.year], (err, res) => {
+//     if (err) console.log(err);
+//     else {
+//       result.complete = res[0].complete
+//     }
+//   })
+//   database.query("select count(progress) as working from project where progress between 1 and 6 and year(deadline) = ?", [data.year], (err, res) => {
+//     if (err) console.log(err);
+//     else {
+//       result.working = res[0].working
+//     }
+//   })
+//   database.query("select count(progress) as delayed from  where  and year(deadline) = ?", [data.year], (err, res) => {
+//     if (err) console.log(err);
+//     else {
+//       result.complete = res[0].complete
+//     }
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- FrontPart/src/views/MyPage.vue
+++ FrontPart/src/views/MyPage.vue
@@ -2,33 +2,28 @@
   <v-card>
     <v-layout>
       <v-app-bar flat density="compact">
-        <v-app-bar-title><div>TODO LIST</div></v-app-bar-title>
+        <v-app-bar-title>
+          <div>TODO LIST</div>
+        </v-app-bar-title>
       </v-app-bar>
     </v-layout>
-      <!-- <li>진행중인 프로젝트.</li>
+    <!-- <li>진행중인 프로젝트.</li>
       <li>todolist, 마감까지 남은 시간(D-OOO)</li>
       <li>작업량 요약본, 도표</li> -->
 
-     <!-- 프로젝트 목록 부분 -->
-    <v-data-table 
-    :headers="mypage_headers" 
-    :items="getData.projects"  
-    :footer-props="{
+    <!-- 프로젝트 목록 부분 -->
+    <v-data-table :headers="mypage_headers" :items="getData.projects" :footer-props="{
       'items-per-page-text': '페이지 당',
       'items-per-page-options': [5, 10, 15, 20],
       'show-current-page': true,
-    }" 
-    class="elevation-0">
+    }" class="elevation-0">
 
-    <!-- 상태 색깔 -->
-    <template v-slot:item.state="{ item }">
-      <v-chip
-        :color="getColor(item.state)"
-        dark
-      >
-        {{ item.state }}
-      </v-chip>
-    </template>
+      <!-- 상태 색깔 -->
+      <template v-slot:item.state="{ item }">
+        <v-chip :color="getColor(item.state)" dark>
+          {{ item.state }}
+        </v-chip>
+      </template>
     </v-data-table>
   </v-card>
 </template>
@@ -41,27 +36,27 @@ import { reactive } from "vue";
 export default {
   data() {
     return {
-    
+
     }
   },
   methods: {
-    getColor (state) {
-        if (state == '완료') return 'green'
-        else if (state == '진행중') return 'blue'
-        else if (state == '취소') return 'yellow'
-        else if (state == '지연') return 'red'
-        else return 'grey'
-      },
+    getColor(state) {
+      if (state == '완료') return 'green'
+      else if (state == '진행중') return 'blue'
+      else if (state == '취소') return 'yellow'
+      else if (state == '지연') return 'red'
+      else return 'grey'
+    },
   },
   computed: {
     ...mapState(["mypage_headers"])
   },
-  setup(){
+  setup() {
     const getData = reactive({
       projects: [],
       users: [],
     });
-    axios.get("/api/project").then((res) => {
+    axios.get("/api/my_project").then((res) => {
       getData.projects = res.data.projects;
     });
     axios.get("/api/user").then((res) => {
